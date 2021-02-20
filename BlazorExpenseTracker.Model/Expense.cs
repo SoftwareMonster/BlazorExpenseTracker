5eaa90c9c038b989377d3a4bfdcdf8f98dac88fc
Monster Software <software@monstersoft.dev>
2021-02-20 12:51:21 +0100

    new: comenzar con la pantalla de gastos

---
+++ BlazorExpenseTracker.Model/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorExpenseTracker.Model
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ExpenseType ExpenseType { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
